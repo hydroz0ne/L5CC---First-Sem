65816be49ae2826484fd51c99961b5c1b72f1119
hydroz0ne <88587169+hydroz0ne@users.noreply.github.com>
2024-01-16 01:40:10 +0400

    13 - Product of list items.py

--- Chapter 1/13 - Product of list items.py
+++ Chapter 1/13 - Product of list items.py
@@ -1,10 +1,22 @@
 #Exercise 13 - Product of list items
 def calcu_product(numlist):
-    product = 1  
+    product = 1
     for value in numlist:
-        product = product * value
+        product *= value
     return product
 
-num = [2, 3, 4, 5]
-result = calcu_product(num)
+numlist = []
+while True:
+    user_input = input("Enter a number (type 'done' to finish): ")
+    
+    if user_input.lower() == 'done':
+        break
+    else:
+        try:
+            num = float(user_input)
+            numlist.append(num)
+        except ValueError:
+            print("Invalid input. Please enter a valid number or 'done' to finish.")
+
+result = calcu_product(numlist)
 print(f"The product of the numbers in the list is {result}.")
